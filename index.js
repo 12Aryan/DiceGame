e3cde1b18afaf3d706d1ca06fc2d452a5e991885
12Aryan <86924503+12Aryan@users.noreply.github.com>
2023-02-04 17:01:24 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -12,13 +12,13 @@ var image2=document.querySelectorAll("img")[1];
 image2.setAttribute("src", randomDiceImage2)
 
 if(randomNumber1>randomNumber2){
-  document.querySelector("h1").innerHTML="Player 1 wins";
+  document.querySelector("h1").innerHTML="🚩 Player 1 Wins!";
 }
 
 else if (randomNumber1<randomNumber2){
-  document.querySelector("h1").innerHTML="Player 2 wins";
+  document.querySelector("h1").innerHTML="Player 2 wins 🚩";
 }
 
 else{
-  document.querySelector("h1").innerHTML="Draw";
+  document.querySelector("h1").innerHTML="Draw!";
 }
